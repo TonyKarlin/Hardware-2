400a51ec347a0b84acdf0cfbfda4c799a6d71044
Tony Karlin <156673907+TonyKarlin@users.noreply.github.com>
2024-10-31 11:26:53 +0200

    sdfsdf

---
+++ Week_2_Slope_Inspection_exercise.py
@@ -0,0 +1,8 @@
+from machine import UART, Pin, I2C, Timer, ADC
+from ssd1306 import SSD1306_I2C
+from filefifo import Filefifo 
+
+
+data = Filefifo(10, name = 'capture_250Hz_01.txt') 
+for _ in range(100): 
+    print(data.get()) 
